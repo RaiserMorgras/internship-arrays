e47285de15003e42fc43ccd57a4eefd904d327e3
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-02-06 12:58:30 +0200

    Adjusted style and replaced while loops with for loops

--- src/subarraysTest/SubarrayCounter.java
+++ src/subarraysTest/SubarrayCounter.java
@@ -2,19 +2,16 @@ package subarraysTest;
 
 public class SubarrayCounter {
     public static int countNegativeSubarrays(int[] array) {
-        int count = 0, begin = 0, end, sum;
-        while (begin < array.length) {
-            //sum = array[begin];
+        int count = 0;
+        int sum;
+        for (int begin = 0; begin < array.length; begin++) {
             sum = 0;
-            end = begin + 1;
-            while (end <= array.length) {
+            for (int end = begin + 1; end <= array.length; end++) {
                 sum += array[end - 1];
                 if (sum < 0) {
                     count++;
                 }
-                end++;
             }
-            begin++;
         }
         return count;
     }
